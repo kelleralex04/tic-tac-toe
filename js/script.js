73bd5ed60cc8c4af1f5d3f7a47feb41c2edd2fe3
Alex Keller <kelleralex04@gmail.com>
2023-11-04 01:19:04 -0600

    complete render functions, add place marker function, add event listeners to handle place marker and reset button, add get winner function with all win checks

--- js/script.js
+++ js/script.js
@@ -12,21 +12,90 @@ let winner;
 
 /*----- cached elements  -----*/
 const messageEl = document.querySelector('h1');
+const playAgainBtn = document.querySelector('button');
 
 /*----- event listeners -----*/
+document.getElementById('board').addEventListener('click', placeMarker)
+playAgainBtn.addEventListener('click', init);
 
 /*----- functions -----*/
 init()
 
 function init() {
     board = [
-        [1, -1, 0],
+        [0, 0, 0],
         [0, 0, 0],
         [0, 0, 0]
     ]
     turn = 1;
     winner = null;
     render();
+}
+
+function placeMarker(evt) {
+    if (winner) {
+        return
+    }
+    colIdx = parseInt(evt.target.getAttribute('id')[1]);
+    rowIdx = parseInt(evt.target.getAttribute('id')[3]);
+    board[colIdx][rowIdx] = turn;
+    turn *= -1;
+    if (board.some((row) => row.includes(0))) {
+        winner = getWinner(colIdx,rowIdx);
+    } else {
+        winner = 'T'
+    }
+    render();
+}
+
+function getWinner(colIdx, rowIdx) {
+        return checkVerticalWin(colIdx, rowIdx) ||
+        checkHorizontalWin(colIdx, rowIdx) ||
+        checkDiagonalWinNESW(colIdx, rowIdx) ||
+        checkDiagonalWinNWSE(colIdx, rowIdx);
+}
+
+function checkDiagonalWinNWSE(colIdx, rowIdx) {
+    const adjCountNW = countAdjacent(colIdx, rowIdx, 1, -1);
+    const adjCountSE = countAdjacent(colIdx, rowIdx, -1, 1);
+    return (adjCountNW + adjCountSE) >= 2 ? board[colIdx][rowIdx] : null;
+}
+
+function checkDiagonalWinNESW(colIdx, rowIdx) {
+    const adjCountNE = countAdjacent(colIdx, rowIdx, 1, 1);
+    const adjCountSW = countAdjacent(colIdx, rowIdx, -1, -1);
+    return (adjCountNE + adjCountSW) >= 2 ? board[colIdx][rowIdx] : null;
+}
+
+function checkHorizontalWin(colIdx, rowIdx) {
+    const adjCountLeft = countAdjacent(colIdx, rowIdx, -1, 0);
+    const adjCountRight = countAdjacent(colIdx, rowIdx, 1, 0);
+    return (adjCountLeft + adjCountRight) >= 2 ? board[colIdx][rowIdx] : null;
+}
+
+function checkVerticalWin(colIdx, rowIdx) {
+    return countAdjacent(colIdx, rowIdx, 0, -1) === 2 ? board[colIdx][rowIdx] : null;
+}
+
+function countAdjacent(colIdx, rowIdx, colOffset, rowOffset) {
+    //  Shortcut variable to the player value
+    const player = board[colIdx][rowIdx];
+    //  Track count of adjacent cells with the same player value
+    let count = 0;
+    //  Initialize new coordinates
+    colIdx += colOffset;
+    rowIdx += rowOffset;
+    while(
+        //  Ensure colIdx is within bounds of the board array
+        board[colIdx] !== undefined &&
+        board[colIdx][rowIdx] !== undefined &&
+        board[colIdx][rowIdx] === player
+    ) {
+        count++;
+        colIdx += colOffset;
+        rowIdx += rowOffset;
+    }
+    return count
 }
 
 function render() {
@@ -56,7 +125,7 @@ function renderBoard() {
 
 function renderMessage() {
     if (winner === 'T') {
-        messageEl.innerText = "It's a Tie!!!";
+        messageEl.innerText = "It's a Tie!";
     } else if (winner) {
         messageEl.innerHTML = `${PLAYERS[winner].toUpperCase()}</span> Wins!`;
         
@@ -67,7 +136,7 @@ function renderMessage() {
 }
 
 function renderControls() {
-
+    playAgainBtn.style.visibility = winner ? 'visible': 'hidden';
 }
 
 
